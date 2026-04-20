c5788d4f0356e58114ee3b97573313d307315d3f
Mukarram Hussain <mukarramhussain30@gmail.com>
2026-04-20 20:47:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package week8;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started");
+        logger.debug("Debug message");
+        logger.error("Error occurred");
+    }
+}
